c72148f1b0d46ac144856484b521ec52a7265c0e
Ame (Adjamé T.R) <Adjame.Tellier-Rozen@etu.univ-grenoble-alpes.fr>
2024-09-11 11:06:29 +0200

    update

--- TD1/TD1_Partie2/TD1_Partie2/obj/Debug/net8.0/TD1_Partie2.AssemblyInfo.cs
+++ TD1/TD1_Partie2/TD1_Partie2/obj/Debug/net8.0/TD1_Partie2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TD1_Partie2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a431122b42997911e9641aec9fcfff686d2267")]
 [assembly: System.Reflection.AssemblyProductAttribute("TD1_Partie2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TD1_Partie2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
